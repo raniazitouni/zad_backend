747e63595fb51ce2fe95b5e7c579df51a87c0326
zitounirania <mr_zitouni@esi.dz>
2025-07-29 01:55:14 +0100

    add front to docker compose

---
+++ inventory/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    # path('Products/', views.Products.as_view(), name='Products'),
+]
--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from models import Product
-from serializers import ProductSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from inventory.models import Product
+# from inventory.serializers import ProductSerializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
 
 #views here 
